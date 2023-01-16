4671a531a1b3d65956140bbf8e503f0c60530d21
Golam Mostofa <golammostofa31188@gmail.com>
2023-01-16 12:27:29 +0600

    test/setup.ts mongo memory server new code format

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -13,8 +13,8 @@ beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
