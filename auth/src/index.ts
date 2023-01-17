6b4b83ad7d31f0384dd1f2aaff08021edbae5e46
Golam Mostofa <golammostofa31188@gmail.com>
2023-01-17 16:32:01 +0600

    auth/src/index.ts modified to see automated deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up..in deployment.');
+  console.log('Starting up..in Digital Ocean Automated Deploment');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
